a787ac8a23ac52b6458dda13597486a1e85e8509
lando10 <orlando.avila@tortoise.dev>
2020-09-22 18:08:10 -0500

    Cambio en el envío de notificación push, ahora conectado a la API de Python.

--- lib/src/models/products_model.dart
+++ lib/src/models/products_model.dart
@@ -56,13 +56,12 @@ class Products {
   }
 
   void sendPush() async {
-    var url = "https://pruebasbotanax.000webhostapp.com/Pedidos/push.php";
+    var url = "http://10.0.2.2:5000//push";
 
     final response = await http.post(url, body: {
       "nombre": nombreCliente,
       "pedido": pedidoFinal,
-      "cantidad":
-          cantidadProducto.toString().replaceAll("[", "").replaceAll("]", ""),
+      "cantidad": cantidadProducto.toString().replaceAll("[", "").replaceAll("]", ""),
       "telefono": telefonoCliente,
       "colonia": coloniaCliente,
       "calle": calleCliente,
